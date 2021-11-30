bed8ab594b708e196f34415479a2fc05b7da4f60
MangeshDevkatte <devmangesh22@gmail.com>
2021-11-30 08:46:12 +0530

    adding ss code

--- TestProject1/src/test/java/TestClass1.java
+++ TestProject1/src/test/java/TestClass1.java
@@ -1,3 +1,11 @@
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestClass1 {
@@ -6,6 +14,17 @@ public class TestClass1 {
 	public void m() {
 		System.out.println("Hello");
 	}
-	
+	@Test
+	public void m1() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "G:\\javaWorkspace\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		
+		File sc= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(sc, new File("G:\\javaWorkspace\\Screenshot\\2.jpg"));
+		
+		driver.close();
+	}
 
 }
